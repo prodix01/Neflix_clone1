83536a168e3946d19b61baf4129356a3ffcc416e
kim <s158614@nishitech.ac.jp>
2020-01-01 13:09:18 +0900

    Add Search container & presenter

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import SearchPresenter from "./SearchPresenter";
+
+class SearchContainer extends Component {
+
+    //상태값
+    state = {
+        movieResults: null,
+        tvResults: null,
+        error: null,
+        loading: false,
+        searchTerm: ""
+    };
+
+    handleSubmit = () => {
+
+    }
+
+    render() {
+
+        const {
+            movieResults,
+            tvResults,
+            error,
+            loading,
+            searchTerm
+        } = this.state;
+        return (
+            <SearchPresenter
+                loading={loading}
+                handleSubmit={this.handleSubmit}
+                movieResults={movieResults}
+                tvResults={tvResults}
+                searchTerm={searchTerm}
+                error={error}
+            />
+        );
+    }
+}
+
+export default SearchContainer;
