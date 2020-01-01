b772b5bba061c41a82b9696b16b76190ec89ffe6
kim <s158614@nishitech.ac.jp>
2020-01-01 11:33:38 +0900

    Add networking and Loader

--- src/Routes/TV/TvContainer.js
+++ src/Routes/TV/TvContainer.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import TvPresenter from "./TvPresenter";
 
+import {tvApi} from "../../api";
+
 class TvContainer extends Component {
 
     //상태값
@@ -10,11 +12,46 @@ class TvContainer extends Component {
         airingToday: null,
         error: null,
         loading: true
-
     };
+
+    //라이프사이클
+    async componentDidMount() {
+        try {
+            const {
+                data: { results: airingToday}
+            } = await tvApi.airingToday();
+
+            const {
+                data: { results: topRate}
+            } = await  tvApi.topRate();
+
+            const {
+                data: { results: popular}
+            } = await tvApi.popular();
+
+            this.setState({
+                airingToday,
+                topRate,
+                popular
+                // popular: popular
+            });
+        }
+        catch {
+            this.setState({
+                error: "Can't find TV show"
+            });
+        }
+        finally {
+            this.setState({
+                loading: false
+            });
+        }
+    }
+
     render() {
 
         const {popular, topRate, airingToday, error, loading} = this.state;
+        console.log(popular);
 
         return (
             <TvPresenter
