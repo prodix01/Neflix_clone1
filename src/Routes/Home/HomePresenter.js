3ae1a6fe4382a9053a1b306cecf75d13ab4d94c0
kim <s158614@nishitech.ac.jp>
2020-01-01 12:29:05 +0900

    Add Section & Poster#1

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,12 +1,59 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Loader from "../../Components/Loader";
+import Section from "../../Components/Section";
+import Poster from "../../Components/Poster";
+import styled from "styled-components";
+
+const Contianer = styled.div`
+    padding: 20px;
+`;
 
 const HomePresenter = ({nowPlaying, popular, upComing, loading, error}) =>
     loading ? (
         <Loader/>
     ) : (
-        <h1>1111</h1>
+        <Contianer>
+            {nowPlaying && nowPlaying.length > 0 && (
+                <Section title="최신영화">
+                    {nowPlaying.map(movie =>
+                        <Poster
+                            id={movie.id}
+                            title={movie.title}
+                            rating={movie.vote_average}
+                            year={movie.release_date}
+                            imageUrl={movie.poster_path}
+                        />
+                    )}
+                </Section>
+            )}
+            {upComing && upComing.length > 0 && (
+                <Section title="개봉예정 영화">
+                    {upComing.map(movie =>
+                        <Poster
+                            id={movie.id}
+                            title={movie.title}
+                            rating={movie.vote_average}
+                            year={movie.release_date}
+                            imageUrl={movie.poster_path}
+                        />
+                    )}
+                </Section>
+            )}
+            {popular && popular.length > 0 && (
+                <Section title="인기영화">
+                    {popular.map(movie =>
+                        <Poster
+                            id={movie.id}
+                            title={movie.title}
+                            rating={movie.vote_average}
+                            year={movie.release_date}
+                            imageUrl={movie.poster_path}
+                        />
+                    )}
+                </Section>
+            )}
+        </Contianer>
     );
 
 HomePresenter.propTypes = {
